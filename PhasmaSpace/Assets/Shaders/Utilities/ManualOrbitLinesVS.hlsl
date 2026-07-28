2f54c51449cbe6eb37f3e704c0c102995bfe9cb6
Christos Karamoustos <chriskarpap@gmail.com>
2026-07-29 02:22:47 +0300

    Sync PhasmaSpace shader copies with DX12 draw-ID vertex input and clip-space Y flip

--- PhasmaSpace/Assets/Shaders/Utilities/ManualOrbitLinesVS.hlsl
+++ PhasmaSpace/Assets/Shaders/Utilities/ManualOrbitLinesVS.hlsl
@@ -1,3 +1,5 @@
+#include "../Common/Structures.hlsl"
+
 struct ManualOrbitPushConstants
 {
     float4x4 viewProjection;
@@ -22,7 +24,7 @@ PS_INPUT_ManualOrbitLine mainVS(VS_INPUT_ManualOrbitLine input)
 {
     PS_INPUT_ManualOrbitLine output;
     float3 worldPosition = input.position + pc.offset.xyz;
-    output.position = mul(float4(worldPosition, 1.0f), pc.viewProjection);
+    output.position = ApplyViewportYConvention(mul(float4(worldPosition, 1.0f), pc.viewProjection));
     output.color = pc.color;
     return output;
 }
